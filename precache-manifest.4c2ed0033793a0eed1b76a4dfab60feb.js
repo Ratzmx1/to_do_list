f9390d86e3bf710e954d10f37df8c78201d8e896
Enzo <enzoxz.023@gmail.com>
2020-08-04 10:40:51 -0400

    Updates

---
+++ precache-manifest.4c2ed0033793a0eed1b76a4dfab60feb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09d2fa45be41a1efd2d911134ee017e1",
+    "url": "/to_do_list/index.html"
+  },
+  {
+    "revision": "2e608c2780ecfd40f9ed",
+    "url": "/to_do_list/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "23ccad8c8531d42b2e7d",
+    "url": "/to_do_list/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2e608c2780ecfd40f9ed",
+    "url": "/to_do_list/static/js/2.afc13f88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to_do_list/static/js/2.afc13f88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23ccad8c8531d42b2e7d",
+    "url": "/to_do_list/static/js/main.34cde5ad.chunk.js"
+  },
+  {
+    "revision": "211d90c18bf12326ba84",
+    "url": "/to_do_list/static/js/runtime-main.c6bad940.js"
+  }
+]);
